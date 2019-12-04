1102f6e0292c1bba2d3204116517f3534b5c58f6
Rio Del S. Pamaong <rpamaong@startechup.com>
2019-12-04 20:19:17 +0800

    initial commit

--- server/server.js
+++ server/server.js
@@ -6,13 +6,19 @@ const resolvers = require('./graphql/resolvers/index');
 const init = async () => {
   // setup database connection either mongo or postgresql
 
+  // setup session authentication
+
   // middlewares
+
+  // logger
 
   const server = new ApolloServer({
     typeDefs,
     resolvers,
     // context: ({ req }) => ({ req, pubsub })
   });
+
+  // error handling
 
   server.listen({ port: 4000 });
 };
