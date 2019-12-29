a15d35b625e90ce2e1586c49d6023d236f76a884
riodel27 <riopamaong@gmail.com>
2019-12-29 23:29:42 +0800

    refactor server database initialization

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,7 @@
-const { ApolloServer, ApolloError } = require('apollo-server-express');
+const {
+  ApolloServer,
+  ApolloError,
+} = require('apollo-server-express');
 
 const app = require('./app');
 const config = require('./config/init');
@@ -6,29 +9,39 @@ const logger = require('./util/logger');
 const resolvers = require('./graphql/resolvers/index');
 const typeDefs = require('./graphql/typeDefs');
 
-const { port } = require('./config');
+const {
+  port,
+} = require('./config');
 
-/** initialize database */
-config.initializeDB();
+(async () => {
+  /** initialize database */
+  await config.initializeDB();
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  playground: true,
-  introspection: true,
-  context: ({ req }) => {
-    // TODO. log request query or mutation
-    // graphiql has a background request like the instropection.
-    // console.log('request body: ', req.body)
-  },
-  formatError: (error) => {
-    if (error.originalError instanceof ApolloError) {
-      logger.error(error);
-    }
-    return error;
-  },
-});
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    playground: true,
+    introspection: true,
+    context: ({
+      req
+    }) => {
+      // TODO. log request query or mutation
+      // graphiql has a background request like the instropection.
+      // console.log('request body: ', req.body)
+    },
+    formatError: (error) => {
+      if (error.originalError instanceof ApolloError) {
+        logger.error(error);
+      }
+      return error;
+    },
+  });
 
-server.applyMiddleware({ app });
+  server.applyMiddleware({
+    app,
+  });
 
-app.listen({ port }, () => logger.info(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`));
+  app.listen({
+    port,
+  }, () => logger.info(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`));
+})();
