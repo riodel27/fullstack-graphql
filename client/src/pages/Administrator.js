56f7a91d52fba1a2a422c97cc8f5222f261b70d8
Rio Del S. Pamaong <rpamaong@startechup.com>
2020-01-29 18:50:33 +0800

    added ui ant design for react

--- client/src/pages/Administrator.js
+++ client/src/pages/Administrator.js
@@ -1,5 +1,8 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/button-has-type */
-import React from "react"
+import React, { useState } from "react"
+import "antd/dist/antd.css"
+import { Button, Divider, Modal, Table } from "antd"
 import { gql, useQuery } from "@apollo/client"
 
 const ADMINISTRATORS = gql`
@@ -12,40 +15,74 @@ const ADMINISTRATORS = gql`
   }
 `
 
-function AdmnistratorList({ administrators }) {
-  return (
-    <ul>
-      {administrators &&
-        administrators.map(({ id, username, email }) => (
-          <li key={id}>
-            <div>name: {username}</div>
-            <div>email: {email}</div>
-            <div>
-              <button>Update</button>
-              <button>Delete</button>
-            </div>
-          </li>
-        ))}
-    </ul>
-  )
-}
-
 function Administrator() {
+  const [newAdminModal, setAddNewModalVisible] = useState(false)
   const { loading, error, data } = useQuery(ADMINISTRATORS)
   const administrators = data && data.administrators
+
   if (loading) return <p>Loading...</p>
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "username",
+      key: "username"
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email"
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: () => (
+        <span>
+          <Button
+            style={{ backgroundColor: "#fa8c16", color: "white" }}
+            onClick={() => setAddNewModalVisible(true)}
+          >
+            Edit
+          </Button>
+          <Divider type="vertical" />
+          <Button type="danger">Delete</Button>
+        </span>
+      )
+    }
+  ]
+
   return (
     <div>
       <h1>Administrator</h1>
-      <button> Add new</button>
-      <AdmnistratorList administrators={administrators} />
+      <Button type="primary" onClick={() => setAddNewModalVisible(true)}>
+        Add new
+      </Button>
+      <Table columns={columns} dataSource={administrators} />
+      <Modal
+        title="Administrator Details"
+        centered
+        visible={newAdminModal}
+        onCancel={() => setAddNewModalVisible(false)}
+        footer={[
+          <Button key="back" onClick={() => setAddNewModalVisible(false)}>
+            Return
+          </Button>,
+          <Button
+            key="submit"
+            type="primary"
+            loading={loading}
+            onClick={() => setAddNewModalVisible(false)}
+          >
+            Save
+            {/* TODO: Form */}
+            {/* TODO: One form for add and update administrator */}
+          </Button>
+        ]}
+      >
+        <p>some contents...</p>
+      </Modal>
     </div>
   )
 }
 
 export default Administrator
-
-// TODO: what are the difference between formik,semantic-ui-react and material ui.
-
-// TODO: model or form for add/update/delete administrator
-// TODO: explore formik and yup validations?
